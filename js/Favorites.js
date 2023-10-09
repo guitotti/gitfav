a092471bc807033b7e41ae547371d9cede98e2e0
Guilherme Totti <dev.guilhermetotti@gmail.com>
2023-10-09 19:20:36 -0300

    generating HTML inside JavaScript

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,6 +1,24 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
+    this.load()
+  }
+
+  load() {
+    this.entries = [
+      {
+        login: 'guitotti',
+        name: 'Guilherme Totti',
+        public_repos: '34',
+        followers: '2',
+      },
+      {
+        login: 'diego3g',
+        name: 'Diego Fernandes',
+        public_repos: '48',
+        followers: '22503',
+      },
+    ]
   }
 }
 
@@ -8,17 +26,56 @@ export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
 
+    this.tbody = this.root.querySelector('table tbody')
+
     this.update()
   }
 
   update() {
     this.removeAllTr()
+
+    this.entries.forEach((user) => {
+      const row = this.createRow()
+
+      row.querySelector(
+        '.user img'
+      ).src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+    tr.innerHTML = `
+        <td class="user">
+           <img src="https://github.com/guitotti.png" alt="Imagem de guitotti">
+           <a href="https://github.com/guitotti" target="_blank">
+             <p>Guilherme Totti</p>
+             <span>guitotti</span>
+           </a>
+         </td>
+         <td class="repositories">
+           34
+         </td>
+         <td class="followers">
+           2
+         </td>
+         <td>
+           <button class="remove">&times;</button>
+         </td>
+        `
+
+    return tr
   }
 
   removeAllTr() {
-    const tbody = this.root.querySelector('table tbody')
-
-    tbody.querySelectorAll('tr').array.forEach((tr) => {
+    this.tbody.querySelectorAll('tr').forEach((tr) => {
       tr.remove()
     })
   }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-import { FavoritesView } from './Favorites'
+import { FavoritesView } from './Favorites.js'
 
 new FavoritesView('#app')
